59c6bff952ecf82f3f7eb7f9277327f3f290cb56
Junjie Yang <yangjunjie0320@gmail.com>
2022-06-30 22:54:23 -0700

    fix bugs

--- src/fci.py
+++ src/fci.py
@@ -127,10 +127,6 @@ def get_fci_matrix_element(config1, config2, h1e, h2e, verbose = False):
     
     elif diff_num == 2:
         if diff_idx_alph is None:
-            if verbose:
-                print("diff_num = 2")
-                print(f"diff_idx_alph is None")
-                assert 1 == 2
 
             mb, nb, pb, qb = get_diff_idx(diff_idx_beta, diff_num)
 
@@ -142,10 +138,6 @@ def get_fci_matrix_element(config1, config2, h1e, h2e, verbose = False):
             h *= (- 1.0) ** sum(occ_beta[min(nb,qb)+1:max(nb,qb)])
 
         elif diff_idx_beta is None:
-            if verbose:
-                print("diff_num = 2")
-                print(f"diff_idx_beta is None")
-                assert 1 == 2
 
             ma, na, pa, qa = get_diff_idx(diff_idx_alph, diff_num)
 
@@ -157,11 +149,6 @@ def get_fci_matrix_element(config1, config2, h1e, h2e, verbose = False):
             h *= (- 1.0) ** sum(occ_alph[min(na, qa)+1:max(na, qa)])
 
         else:
-            if verbose:
-                print("diff_num = 2")
-                print(f"diff_idx_alph is not None")
-                print(f"diff_idx_beta is not None")
-                assert 1 == 2
                 
             ma, pa = get_diff_idx(diff_idx_alph, 1)
             nb, qb = get_diff_idx(diff_idx_beta, 1)
